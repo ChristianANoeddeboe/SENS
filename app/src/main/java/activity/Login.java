ca9f02e5f7ec0bea261cb89102c7673f289a590f
Thyge Steffensen <s175176@student.dtu.dk>
2018-10-25 11:20:40 +0200

    Login done?

--- app/src/main/java/activity/Login.java
+++ app/src/main/java/activity/Login.java
@@ -1,8 +1,13 @@
 package activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.root.sens.MainActivity;
 import com.example.root.sens.R;
 
 public class Login extends AppCompatActivity {
@@ -11,5 +16,17 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button login = findViewById(R.id.login_btn);
+        Button help = findViewById(R.id.help_btn);
+
+        login.setOnClickListener((View v) ->{
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(i);
+        });
+
+        help.setOnClickListener((View v) ->{
+            Toast.makeText(this, "Not implemented yet!", Toast.LENGTH_LONG).show();
+        });
     }
 }
