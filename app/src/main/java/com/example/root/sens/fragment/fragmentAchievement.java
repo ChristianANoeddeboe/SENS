cebeedf7d65da0a35b2b66824715b0cba95545ff
Christian Andersen <angercraft@gmail.com>
2018-10-25 16:29:17 +0200

    Added achievements. Co-authored-by: mathiasthejsen thejsen@live.dk

--- app/src/main/java/com/example/root/sens/fragment/fragmentAchievement.java
+++ app/src/main/java/com/example/root/sens/fragment/fragmentAchievement.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 import com.example.root.sens.R;
 
@@ -64,8 +66,22 @@ public class fragmentAchievement extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_achievement, container, false);
+        String[] lande = {"Danmark", "Norge", "Sverige", "Finland",
+                "Holland", "Italien", "Tyskland", "Frankrig", "Spanien", "Portugal",
+                "Nepal", "Indien", "Kina", "Japan", "Thailand",
+                "Danmark", "Norge", "Sverige", "Finland",
+                "Holland", "Italien", "Tyskland", "Frankrig", "Spanien", "Portugal",
+                "Nepal", "Indien", "Kina", "Japan", "Thailand"};
+
+        ArrayAdapter adapter = new ArrayAdapter(v.getContext(), R.layout.achievement_list_element, R.id.listelement_title, lande);
+
+        GridView gridView = v.findViewById(R.id.achieve_gridview);
+
+        gridView.setAdapter(adapter);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_achievement, container, false);
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
