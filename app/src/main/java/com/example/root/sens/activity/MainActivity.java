a00efb7b4ba8f15a9f7c9ab3c69b8ee31772083c
Christian Andersen <angercraft@gmail.com>
2018-10-25 12:32:21 +0200

    Added viewpager and viewpager indicator. Added fragments for each function.

--- app/src/main/java/com/example/root/sens/activity/MainActivity.java
+++ app/src/main/java/com/example/root/sens/activity/MainActivity.java
@@ -1,8 +1,12 @@
-package com.example.root.sens;
+package com.example.root.sens.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,24 +17,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.astuetz.PagerSlidingTabStrip;
+import com.example.root.sens.R;
+import com.example.root.sens.fragment.fragmentAchievement;
+import com.example.root.sens.fragment.fragmentGame;
+import com.example.root.sens.fragment.fragmentGoals;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private ViewPager viewPager;
+    private static String[] viewNames = {"Trofæer", "Mål", "Spil"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,7 +43,16 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-    }
+
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(new ViewpagerAdapter(getSupportFragmentManager()));
+        // viewPager.setPageTransformer(false, new ZoomOutPageTransformer());
+
+        PagerSlidingTabStrip pagerSlidingTabStrip = findViewById(R.id.pagerTitleStrip);
+        pagerSlidingTabStrip.setShouldExpand(true);
+        pagerSlidingTabStrip.setIndicatorColorResource(R.color.sensBlue);
+        pagerSlidingTabStrip.setViewPager(viewPager);
+           }
 
     @Override
     public void onBackPressed() {
@@ -65,12 +77,12 @@ public class MainActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+/*
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
+*/
         return super.onOptionsItemSelected(item);
     }
 
@@ -83,7 +95,10 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-
+            Fragment fragment = new fragmentGame();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, fragment)  // tom container i layout
+                    .commit();
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
@@ -97,5 +112,34 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private class ViewpagerAdapter extends FragmentPagerAdapter {
+
+
+        public ViewpagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return viewNames[position];
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            Fragment f = null;
+            if (i == 0) f = new fragmentAchievement();
+            else if (i == 1) f = new fragmentGoals();
+            else f = new fragmentGame();
+
+            return f;
+
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
     }
 }
