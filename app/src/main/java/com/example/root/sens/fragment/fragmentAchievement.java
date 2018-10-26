1f41ad11a3584dc83340850dc1be4c3889b3a144
Mathias Thejsen <thejsen@live.dk>
2018-10-26 14:16:55 +0200

    Added goals

--- app/src/main/java/com/example/root/sens/fragment/fragmentAchievement.java
+++ app/src/main/java/com/example/root/sens/fragment/fragmentAchievement.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ListView;
 
 import com.example.root.sens.R;
 
