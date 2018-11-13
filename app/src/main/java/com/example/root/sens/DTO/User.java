193c3d92fedb49fe0322b45de4b3b8235780a0f4
mathiasthejsen <thejsen@live.dk>
2018-11-13 08:35:22 +0100

    Added user DTO

--- app/src/main/java/com/example/root/sens/DTO/User.java
+++ app/src/main/java/com/example/root/sens/DTO/User.java
@@ -1,12 +1,19 @@
 package com.example.root.sens.DTO;
 
+import java.util.Date;
+import java.util.HashMap;
+
 public class User {
     private int id;
     private String name;
+    private Date fødselsdag;
+    private HashMap<String, Integer> goals;
 
-    public User(int id, String name) {
+    public User(int id, String name, Date fødselsdag, HashMap<String, Integer> goals) {
         this.id = id;
         this.name = name;
+        this.fødselsdag = fødselsdag;
+        this.goals = goals;
     }
 
     public int getId() {
@@ -23,5 +30,21 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Date getFødselsdag() {
+        return fødselsdag;
+    }
+
+    public void setFødselsdag(Date fødselsdag) {
+        this.fødselsdag = fødselsdag;
+    }
+
+    public HashMap<String, Integer> getGoals() {
+        return goals;
+    }
+
+    public void setGoals(HashMap<String, Integer> goals) {
+        this.goals = goals;
     }
 }
