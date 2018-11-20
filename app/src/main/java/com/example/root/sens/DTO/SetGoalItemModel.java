114a832f8aa4c0bb33153257beddf9bc0b717fb0
Thyge Steffensen <s175176@student.dtu.dk>
2018-11-20 11:47:05 +0100

    Bekræftelse af data
    Co-authored-by: JeppeTrip <s175197@student.dtu.dk>

---
+++ app/src/main/java/com/example/root/sens/DTO/SetGoalItemModel.java
@@ -0,0 +1,13 @@
+package com.example.root.sens.DTO;
+
+public class SetGoalItemModel {
+    private String primaryTxt;
+
+    public SetGoalItemModel(String primaryTxt){
+        this.primaryTxt = primaryTxt;
+    }
+
+    public String getPrimaryTxt() {
+        return primaryTxt;
+    }
+}
--- app/src/main/java/com/example/root/sens/fragment/UserConfigGoalInfoFragment.java
+++ app/src/main/java/com/example/root/sens/fragment/UserConfigGoalInfoFragment.java
@@ -2,20 +2,58 @@ package com.example.root.sens.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.root.sens.DTO.ItemModel;
+import com.example.root.sens.DTO.SetGoalItemModel;
 import com.example.root.sens.R;
+import com.example.root.sens.adapter.SetGoalAdapter;
+import com.example.root.sens.adapter.SettingsAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserConfigGoalInfoFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            ViewGroup rootView = (ViewGroup) inflater.inflate(
-                    R.layout.user_config_f_viewpager_goal_info, container, false);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.user_config_f_viewpager_goal_info, container, false);
 
-            return rootView;
-        }
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView_set_goal);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        // mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new SetGoalAdapter(createItem());
+        mRecyclerView.setAdapter(mAdapter);
+
+        return rootView;
     }
+
+    private List<SetGoalItemModel> createItem() {
+        ArrayList<SetGoalItemModel> items = new ArrayList<SetGoalItemModel>();
+
+        items.add(new SetGoalItemModel("Cykling"));
+        items.add(new SetGoalItemModel("Gang"));
+        items.add(new SetGoalItemModel("Træning"));
+        items.add(new SetGoalItemModel("Stå"));
+        items.add(new SetGoalItemModel("Anden bevægelse"));
+
+        return items;
+    }
+}
