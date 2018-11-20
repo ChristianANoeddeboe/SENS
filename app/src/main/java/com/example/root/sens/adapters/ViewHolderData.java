102d12b1a23291b8d60f3464f659a38d10c409c6
Christian Andersen <angercraft@gmail.com>
2018-11-20 08:23:40 +0100

    Added hardcoded graph lines.

--- app/src/main/java/com/example/root/sens/adapters/ViewHolderData.java
+++ app/src/main/java/com/example/root/sens/adapters/ViewHolderData.java
@@ -1,8 +1,8 @@
 package com.example.root.sens.adapters;
 
 import android.graphics.Color;
+import android.graphics.PointF;
 import android.view.View;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.root.sens.DTO.DayData;
@@ -14,18 +14,16 @@ import com.example.root.sens.view.fragments.interfaces.TypeData;
 import com.hookedonplay.decoviewlib.DecoView;
 import com.hookedonplay.decoviewlib.charts.EdgeDetail;
 import com.hookedonplay.decoviewlib.charts.SeriesItem;
-import com.hookedonplay.decoviewlib.events.DecoEvent;
 
 import java.util.Calendar;
-import java.util.Date;
 
 public class ViewHolderData extends ViewHolder {
     private final TextView mTextView;
-    private final DecoView mProgressBar;
+    private final DecoView progressCircle;
     public ViewHolderData(View itemView) {
         super(itemView);
         mTextView = itemView.findViewById(R.id.header);
-        mProgressBar = itemView.findViewById(R.id.dynamicArcView);
+        progressCircle = itemView.findViewById(R.id.dynamicArcView);
     }
 
     public void bindType(ListItem item) {
@@ -55,16 +53,45 @@ public class ViewHolderData extends ViewHolder {
                }
                mTextView.setText(g.getType().getName()+" : "+temp.intValue()+"/"+g.getValue()+" timer");
                //https://github.com/bmarrdev/android-DecoView-charting
-               mProgressBar.addSeries(new SeriesItem.Builder(Color.argb(255, 218, 218, 218))
+               PointF point = new PointF(-20f, -20f);
+
+               progressCircle.addSeries(new SeriesItem.Builder(Color.argb(255, 218, 218, 218))
+                       .setRange(0, 100, 100)
+                       .setLineWidth(64f)
+                       .build());
+
+               progressCircle.addSeries(new SeriesItem.Builder(Color.argb(255, 64, 196, 0))
+                       .setRange(0, 100, 33)
+                       .setLineWidth(24f)
+                       .setInset(point)
+                       .build());
+               point = new PointF(point.x+20, point.y+20);
+
+               progressCircle.addSeries(new SeriesItem.Builder(Color.argb(255, 0, 0, 255))
+                       .setRange(0, 100, 50)
+                       .setLineWidth(24f)
+                       .setInset(point)
+                       .build());
+
+               point = new PointF(point.x+20, point.y+20);
+               progressCircle.addSeries(new SeriesItem.Builder(Color.argb(255, 255, 0, 0))
+                       .setRange(0, 100, 75)
+                       .setLineWidth(24f)
+                       .setInset(point)
+                       .build());
+
+               /*
+               progressCircle.addSeries(new SeriesItem.Builder(Color.argb(255, 218, 218, 218))
                        .setRange(0, 100, 100)
                        .setInitialVisibility(true)
                        .setLineWidth(32f)
                        .build());
-               mProgressBar.addSeries(new SeriesItem.Builder(Color.argb(255, 64, 196, 0))
+               progressCircle.addSeries(new SeriesItem.Builder(Color.argb(255, 64, 196, 0))
                        .setRange(0, 1440, temp.intValue())
                        .setLineWidth(32f)
                        .addEdgeDetail(new EdgeDetail(EdgeDetail.EdgeType.EDGE_OUTER, Color.parseColor("#22000000"), 0.4f))
                        .build());
+               */
 
 
 
