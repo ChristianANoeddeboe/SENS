b84bc5706ad72bc4af0b1f4b39a28a2ce32a2f23
mathiasthejsen <thejsen@live.dk>
2018-11-22 10:31:15 +0100

    More work on realm

--- app/src/main/java/com/example/root/sens/DAO/GoalDAO.java
+++ app/src/main/java/com/example/root/sens/DAO/GoalDAO.java
@@ -6,19 +6,20 @@ import com.example.root.sens.DTO.User;
 import io.realm.Realm;
 import io.realm.RealmList;
 import io.realm.RealmResults;
+import io.realm.exceptions.RealmPrimaryKeyConstraintException;
 
 public class GoalDAO{
     public void saveGoalHistory(User user){
         Realm realm = Realm.getDefaultInstance();
         realm.beginTransaction();
-        realm.insert(user.getGoals());
+        realm.copyToRealmOrUpdate(user.getGoals());
         realm.commitTransaction();
-
     }
 
     public GoalHistory loadGoalHistory(User user){
         Realm realm = Realm.getDefaultInstance();
         final RealmResults<GoalHistory> goalHistory = realm.where(GoalHistory.class).findAll();
+        GoalHistory g = goalHistory.first();
         return goalHistory.first();
     }
 
