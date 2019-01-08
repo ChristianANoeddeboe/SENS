d82a86365d5743f2876e42110375a1c950638d6d
Thyge Steffensen <s175176@student.dtu.dk>
2019-01-08 09:56:13 +0100

    Prøvede at sætte en Broadcast receiver op, samt en service. Dog virker det ikke helt. Vi laver proof of concept i stedet
    Co-authored-by: JeppeTrip <s175197@student.dtu.dk>

--- app/src/main/java/com/example/root/sens/activities/Login.java
+++ app/src/main/java/com/example/root/sens/activities/Login.java
@@ -1,5 +1,8 @@
 package com.example.root.sens.activities;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +16,7 @@ import com.example.root.sens.controllers.interfaces.ILoginController;
 import com.example.root.sens.R;
 import com.example.root.sens.controllers.LoginController;
 import com.example.root.sens.notification.NotificationsManager;
+import com.example.root.sens.notification.TimeReceiver;
 
 public class Login extends AppCompatActivity {
     private final static String TAG = Login.class.getSimpleName();
@@ -29,7 +33,19 @@ public class Login extends AppCompatActivity {
 
         setContentView(R.layout.activity_login);
 
-        NotificationsManager noti = new NotificationsManager("42", this);
+
+        Intent notifyIntent = new Intent(this, TimeReceiver.class);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast
+                (this, 42, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,  System.currentTimeMillis(),
+                AlarmManager.INTERVAL_FIFTEEN_MINUTES, pendingIntent);
+
+
+
 
 
         EditText sensorField = findViewById(R.id.key_text);
