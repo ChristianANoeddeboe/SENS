00f815e61f59d8ceaf840c36a30a5c66d58f26c5
Thyge Steffensen <s175176@student.dtu.dk>
2019-01-09 15:20:47 +0100

    Updated layout to fulfill Material IO standards

--- app/src/main/java/com/example/root/sens/fragments/LoginHelpFragment.java
+++ app/src/main/java/com/example/root/sens/fragments/LoginHelpFragment.java
@@ -1,20 +1,15 @@
 package com.example.root.sens.fragments;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
-import android.widget.ImageButton;
 
 import com.example.root.sens.R;
-import com.example.root.sens.recyclers.adapter.AboutAdapter;
-import com.example.root.sens.recyclers.itemmodels.AboutItemModel;
-
-import java.util.ArrayList;
 
 public class LoginHelpFragment extends Fragment{
 
@@ -22,10 +17,12 @@ public class LoginHelpFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_login_help, container, false);
 
-        FrameLayout frameLayout = rootView.findViewById(R.id.frameLayout_login_help);
-        frameLayout.setOnClickListener((View v) -> {
-            getActivity().onBackPressed();
-        });
+        ConstraintLayout constraintLayout = rootView.findViewById(R.id.layout_login_help);
+        constraintLayout.setOnClickListener((View v) -> getActivity().onBackPressed());
+
+        Button exitButton = rootView.findViewById(R.id.button_login_helper_exit);
+        exitButton.setOnClickListener((View v) -> getActivity().onBackPressed());
+
 
         return rootView;
     }
