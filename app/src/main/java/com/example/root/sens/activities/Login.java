b5f51dfcf1bae463a84ac781adf809efa3a97505
Thyge Steffensen <s175176@student.dtu.dk>
2019-01-09 12:20:10 +0100

    Login dokumenteret

--- app/src/main/java/com/example/root/sens/activities/Login.java
+++ app/src/main/java/com/example/root/sens/activities/Login.java
@@ -1,8 +1,5 @@
 package com.example.root.sens.activities;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,16 +8,13 @@ import android.transition.Slide;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.example.root.sens.controllers.interfaces.ILoginController;
 import com.example.root.sens.R;
 import com.example.root.sens.controllers.LoginController;
 import com.example.root.sens.fragments.LoginHelpFragment;
-import com.example.root.sens.notification.TimeReceiver;
 
 public class Login extends AppCompatActivity {
-    private final static String TAG = Login.class.getSimpleName();
     ILoginController loginController = new LoginController();
 
     @Override
@@ -32,14 +26,16 @@ public class Login extends AppCompatActivity {
         EditText sensorField = findViewById(R.id.key_text);
         Button login = findViewById(R.id.login_btn);
         Button help = findViewById(R.id.help_btn);
-        sensorField.setText("17-3B.BA");
+
+        sensorField.setText("17-3B.BA"); // Sættes da appen bruges til fremvisning
+        
         login.setOnClickListener((View v) -> {
             String sensorID = String.valueOf(sensorField.getText());
 
             if (sensorID.length() == 0) {
                 Snackbar.make(findViewById(R.id.login_coordinator_layout),
                         "Du skal angive et sensor id for at fortsætte",
-                        Snackbar.LENGTH_SHORT).show();
+                        Snackbar.LENGTH_LONG).show();
                 return;
             }
 
@@ -51,7 +47,7 @@ public class Login extends AppCompatActivity {
             } else {
                 Snackbar.make(findViewById(R.id.login_coordinator_layout),
                         "Brugeren er ikke fundet!",
-                        Snackbar.LENGTH_SHORT).show();
+                        Snackbar.LENGTH_LONG).show();
             }
 
             Intent i = new Intent(getApplicationContext(), UserConfigActivity.class);
