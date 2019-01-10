9ded28398dd7bfe74414896bb31150864984640e
Mathias Thejsen <thejsen@live.dk>
2019-01-10 12:07:44 +0100

    Created new fragment for updating goals

--- app/src/main/java/com/example/root/sens/activities/ManageGoalActivity.java
+++ app/src/main/java/com/example/root/sens/activities/ManageGoalActivity.java
@@ -4,22 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.root.sens.R;
+import com.example.root.sens.fragments.ManageGoalFragment;
 import com.example.root.sens.fragments.UserConfigGoalInfoFragment;
 
 
 public class ManageGoalActivity extends AppCompatActivity {
 
     private final String TAG = this.getClass().getSimpleName();
-    UserConfigGoalInfoFragment goalInfoFragment = new UserConfigGoalInfoFragment();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_goal);
 
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.managegoal_FrameLayout_container, goalInfoFragment)
-                .addToBackStack(null)
+                .replace(R.id.managegoal_FrameLayout_container, new ManageGoalFragment())
                 .commit();
 
 
