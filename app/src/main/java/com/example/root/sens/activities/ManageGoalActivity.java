225ef5246ccd3949ef0d54d5ca1d433954a49e83
christian <s164150@student.dtu.dk>
2019-01-10 12:09:02 +0100

    Merge branch 'goalmanagement' of https://github.com/mathiasthejsen/SENS into goalmanagement
    # Conflicts:
    #	app/src/main/java/com/example/root/sens/activities/ManageGoalActivity.java

--- app/src/main/java/com/example/root/sens/activities/ManageGoalActivity.java
+++ app/src/main/java/com/example/root/sens/activities/ManageGoalActivity.java
@@ -5,12 +5,10 @@ import android.os.Bundle;
 
 import com.example.root.sens.R;
 import com.example.root.sens.fragments.ManageGoalFragment;
-import com.example.root.sens.fragments.UserConfigGoalInfoFragment;
 
 
 public class ManageGoalActivity extends AppCompatActivity {
 
-    private final String TAG = this.getClass().getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
