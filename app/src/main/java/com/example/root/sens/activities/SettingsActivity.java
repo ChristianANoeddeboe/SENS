d65920c8403a11c4250c5db348a982ccad9c94ae
christian <s164150@student.dtu.dk>
2019-01-15 09:43:25 +0100

    Added setting button for fetching data on demand.

--- app/src/main/java/com/example/root/sens/activities/SettingsActivity.java
+++ app/src/main/java/com/example/root/sens/activities/SettingsActivity.java
@@ -7,9 +7,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.example.root.sens.R;
+import com.example.root.sens.dao.SensDAO;
 import com.example.root.sens.dao.UserDAO;
+import com.example.root.sens.dao.interfaces.DatabaseObserver;
+import com.example.root.sens.dao.interfaces.SensObserver;
+import com.example.root.sens.dao.interfaces.SensSubject;
 import com.example.root.sens.dto.DayData;
 import com.example.root.sens.dto.Record;
 import com.example.root.sens.recyclers.adapters.intefaces.ItemClickListener;
@@ -22,10 +28,12 @@ import java.util.List;
 import io.realm.Realm;
 import io.realm.RealmList;
 
-public class SettingsActivity extends AppCompatActivity implements ItemClickListener {
+public class SettingsActivity extends AppCompatActivity implements ItemClickListener, SensObserver, DatabaseObserver {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
+    private Snackbar snackbar;
+    private SensSubject sensSubject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +54,7 @@ public class SettingsActivity extends AppCompatActivity implements ItemClickList
         ArrayList<ItemModel> items = new ArrayList<>();
         items.add(new ItemModel("Ryd data",
                 "Dette vil fjerne alt dine data!", R.drawable.ic_trash_alt));
-        items.add(new ItemModel("Entry " + 1, R.mipmap.ic_launcher));
+        items.add(new ItemModel("Hent data", "Henter ny data fra sensoren.", R.drawable.ic_reset_black));
         items.add(new ItemModel("Entry " + 2, R.mipmap.ic_launcher));
         items.add(new ItemModel("Entry " + 3, R.mipmap.ic_launcher));
         items.add(new ItemModel("Entry " + 4, R.mipmap.ic_launcher));
@@ -97,12 +105,39 @@ public class SettingsActivity extends AppCompatActivity implements ItemClickList
                 current.show();
 
                 break;
+            case 1:
+                sensSubject = SensDAO.getInstance();
+                sensSubject.registerObserver(this); // We register this view as an observer, this is used for when fetching data from SENS
+                SensDAO.getInstance().getData(getString(R.string.SensPatientKey), 14);
+                snackbar = Snackbar.make(recyclerView, R.string.DownloadingData, Snackbar.LENGTH_INDEFINITE);
+                ViewGroup contentLay = (ViewGroup) snackbar.getView().findViewById(android.support.design.R.id.snackbar_text).getParent();
+                ProgressBar progressBar = new ProgressBar(this);
+                contentLay.addView(progressBar,0);
+                snackbar.show();
+                break;
+
             default:
                 Snackbar.make(findViewById(R.id.settings_layout),
                         R.string.NotYetImplemented,
                         Snackbar.LENGTH_LONG).show();
                 break;
         }
+
+    }
+
+    @Override
+    public void onDataReceived() {
+        snackbar.dismiss();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        sensSubject.removeObserver(this);
+    }
+
+    @Override
+    public void onDataChanged() {
 
     }
 }
