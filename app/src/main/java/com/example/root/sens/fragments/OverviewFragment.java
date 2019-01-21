b0feb2b12f8e2c955f8b934e5f63570f365854a1
Jeppe <s175197@student.dtu.dk>
2019-01-21 22:19:01 +0100

    fikset en bug hvor overview siden ikke opdaterede

--- app/src/main/java/com/example/root/sens/fragments/OverviewFragment.java
+++ app/src/main/java/com/example/root/sens/fragments/OverviewFragment.java
@@ -10,12 +10,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.root.sens.R;
+import com.example.root.sens.dao.SensDAO;
+import com.example.root.sens.dao.UserDAO;
+import com.example.root.sens.dao.interfaces.DatabaseObserver;
+import com.example.root.sens.dao.interfaces.DatabaseSubject;
+import com.example.root.sens.dao.interfaces.SensObserver;
+import com.example.root.sens.dao.interfaces.SensSubject;
 import com.example.root.sens.recyclers.adapters.OverviewAdapter;
 
 import java.util.Date;
 
 
-public class OverviewFragment extends Fragment {
+public class OverviewFragment extends Fragment implements SensObserver, DatabaseObserver {
+    SensSubject sensSubject;
+    DatabaseSubject databaseSubject;
+    OverviewAdapter overviewAdapter;
+
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -25,9 +35,30 @@ public class OverviewFragment extends Fragment {
         RecyclerView recyclerView = v.findViewById(R.id.goalsRecycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(v.getContext()));
 
-        OverviewAdapter overviewAdapter = new OverviewAdapter(this.getContext(), new Date(), true);
+        overviewAdapter = new OverviewAdapter(this.getContext(), new Date(), true);
         recyclerView.setAdapter(overviewAdapter);
 
+        sensSubject = SensDAO.getInstance();
+        sensSubject.registerObserver(this);
+        databaseSubject = UserDAO.getInstance();
+        databaseSubject.registerObserver(this);
         return v;
+    }
+
+    @Override
+    public void onDataReceived(boolean b) {
+        overviewAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onDataChanged() {
+        overviewAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        databaseSubject.removeObserver(this);
+        sensSubject.removeObserver(this);
     }
 }
